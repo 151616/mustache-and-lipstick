cd37353eb0748e9ba1327989ed1487363796397c
shivansh payak <75551006+151616@users.noreply.github.com>
2021-08-21 13:53:46 -0400

    Add files via upload

---
+++ lipstick.js
@@ -0,0 +1,41 @@
+mouthx = 0;
+mouthy = 0;
+function preload(){
+lipstick = loadImage("lipstick.png");
+}
+function setup(){
+    canvas = createCanvas(400,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 300);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotposes);
+}
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         mouth= results[0].pose.mouth.x;
+        mouthy = results[0].pose.mouth.y;
+        console.log("mouth x= " + mouthex);
+        console.log("mouth y=" + mouthy);
+    }
+}
+function modelLoaded(){
+    console.log("Posenet is Initialized");
+}
+function draw(){
+image(video, 0, 0, 400, 300);
+image(lipstick, mouthx, mouthy, 30, 30);
+
+
+}
+function take_snapshot(){
+    save("Lipstick.png");
+}
+function lipstick(){
+    window.location = "lipstick.html";
+}
+function mustache(){
+    window.location = "mustache.html";
+}
